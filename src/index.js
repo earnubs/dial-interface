581f5fcdb6464e4c1a2836323ebae2e3d6d12e3c
Stephen Stewart <stephen.stewart@carisenda.com>
2021-01-02 16:47:29 +0000

    Ignore build directory

--- src/index.js
+++ src/index.js
@@ -1,218 +1,72 @@
 /* eslint-disable no-console */
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Worker from './rotation.worker.js';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
 
+import Dial from "./components/Dial";
+import Panel from "./components/Panel";
+//import Cube from './components/Cube';
 
-/** props: value, maximum, minimum, width, height **/
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
 
-const propTypes = {
-  initialRotation: PropTypes.number, // TODO
-  display: PropTypes.bool,
-  radius: PropTypes.number.isRequired,
-  resistance: PropTypes.number.isRequired,
-};
+const random = () => Math.floor(Math.random() * Math.random() * 360);
 
-const defaultProps = {
-  display: true,
-};
+const DialAndDisplay = styled.div`
+  position: relative;
+`;
 
-export default class Dial extends Component {
+const Display = styled.div`
+  position: absolute;
+  width: 40px;
+  height: 16px;
+  margin: -8px 0 0 -20px;
+  top: 50%;
+  left: 50%;
+  z-index: 0;
+  line-height: 16px;
+  text-align: center;
+  font-size: 16px;
+  color: #f6ffcb;
+  font-family: "Blender Pro", sans-serif;
+  font-weight: 400;
+  pointer-events: none;
+`;
 
-  constructor(props) {
-    super(props);
+const DialWithOutput = props => {
+  const [value, setValue] = useState(props.initialRotation);
 
-    this.worker = new Worker();
-    this.boundHandleClick = this.handleClick.bind(this);
-    this.boundHandleKeyDown = this.handleKeyDown.bind(this);
-    this.boundHandleKeyUp = this.handleKeyUp.bind(this);
-    this.boundHandleMouseMove = this.handleMouseMove.bind(this);
-    this.boundHandleTouchMove = this.handleTouchMove.bind(this);
-    this.boundHandleEnter = this.handleEnter.bind(this);
-    this.boundHandleLeave = this.handleLeave.bind(this);
-    this.state = {
-      rotation: 0,
-      isActive: false
-    };
+  const changeHandler = value => {
+    setValue(value)
   }
 
-  componentDidMount() {
-    this._values = {
-      previous: {},
-      offset: getOffset(this.dialEl)
-    };
-
-    this.worker.addEventListener('message', event => {
-      if (this.state.isActive) {
-        const { degrees, hit } = JSON.parse(event.data);
-        this.setState({
-          rotation: this.state.rotation + degrees,
-        });
-        this._values.previous.X = hit[0];
-        this._values.previous.Y = hit[1];
-      }
-    });
-  }
-
-  render() {
-    const { radius } = this.props;
-    const { isActive } = this.state;
-    const diameter = radius * 2;
-    const wrapperStyle = {
-      width: diameter,
-      height: diameter,
-      position: 'relative',
-    };
-    const dialStyle = {
-      WebkitTransform: 'rotate(' + this.state.rotation + 'deg)',
-      width: diameter,
-      height:diameter,
-      position: 'absolute'
-    };
-
-    return (
-      <div
-        className="wrapper" style={ wrapperStyle }
-        onClick={ this.boundHandleClick }
-        onKeyDown={ this.boundHandleKeyDown }
-        onKeyUp={ this.boundHandleKeyUp }
-      >
-        <div
-          className={ `dial${ isActive ? ' isActive' : ''}`}
-          onMouseEnter={ this.boundHandleEnter }
-          onMouseLeave={ this.boundHandleLeave }
-          onMouseMove={ this.boundHandleMouseMove }
-          onTouchMove={ this.boundHandleTouchMove }
-          onTouchEnd={ this.boundHandleLeave }
-          ref={ (dial) => { this.dialEl = dial; }}
-          style={ dialStyle }
-          tabIndex={1}
-        />
-        <span className="value">{ this.props.display && this.state.rotation | 0 }</span>
-      </div>
-    );
-  }
-
-  handleClick() {
-    //
-  }
-
-  handleKeyDown(e) {
-    const { keyCode, shiftKey } = e.nativeEvent;
-
-    if (keyCode === 16) {
-      this.setState({
-        isActive: true
-      });
-    }
-
-    if (keyCode === 38) {
-      this.setState({
-        rotation: this.state.rotation + this.arrowInc(shiftKey)
-      });
-    }
-
-    if (keyCode === 40) {
-      this.setState({
-        rotation: this.state.rotation - this.arrowInc(shiftKey)
-      });
-    }
-  }
-
-  handleKeyUp(e) {
-    const { keyCode } = e.nativeEvent;
-
-    if (keyCode === 16) {
-      this.setState({
-        isActive: false
-      });
-    }
-  }
-
-  arrowInc(shiftKey) {
-    return shiftKey ? 10 : 1;
-  }
-
-  handleEnter() {
-    this.dialEl.focus();
-  }
-
-  handleLeave() {
-    this.dialEl.blur();
-    this.setState({
-      isActive: false
-    });
-    this._values.previous = {};
-  }
-
-  handleMouseMove(e) {
-    e.preventDefault();
-
-    //if (!this.state.isActive) return;
-    this.handleMovement(e.pageX, e.pageY);
-  }
-
-  handleTouchMove(e) {
-    e.preventDefault();
-
-    //if (!this.state.isActive) return;
-    this.handleMovement(e.touches[0].pageX, e.touches[0].pageY);
-  }
-
-  handleMovement(pageX, pageY) {
-    const { radius, resistance } = this.props;
-    const {
-      previous,
-      offset
-    } = this._values;
-
-    this.worker.postMessage(JSON.stringify({
-      radius,
-      resistance,
-      offset,
-      page: [pageX, pageY], // cursor, pointer or touch
-      previous
-    }));
-  }
+  return (
+    <DialAndDisplay>
+    <Dial {...props} onChange={changeHandler} />
+    <Display>{value}</Display>
+    </DialAndDisplay>
+  )
 }
-
-Dial.propTypes = propTypes;
-Dial.defaultProps = defaultProps;
 
 ReactDOM.render(
-  <div>
-    <Dial radius={40} resistance={75} />
-    <Dial radius={40} resistance={150} />
-    <Dial radius={40} resistance={10} />
-    <Dial radius={40} resistance={300} />
-  </div>,
-  document.getElementById('content')
+  <Wrapper>
+    {Array(9)
+      .fill(0)
+      .map((_, i) => (
+        <Panel key={i}>
+          {Array(5)
+            .fill(0)
+            .map((_, i) => (
+              <DialWithOutput
+                key={i}
+                radius={40}
+                initialRotation={random()}
+              />
+            ))}
+        </Panel>
+      ))}
+  </Wrapper>,
+  document.getElementById("root")
 );
-
-export function getOffset(el) {
-  el = el.getBoundingClientRect();
-
-  return [
-    el.left + window.scrollX,
-    el.top + window.scrollY
-  ];
-}
-
-/**
-function anglebetweenvectors(a, b) {
-  return Math.acos(
-    dotproduct(a, b) / (magnitude(a) * magnitude(b))) * (180 / Math.PI);
-}
-
-function degminsec(deg) {
-  const degInt = deg | 0;
-  const min = 60 * (deg - degInt);
-  const minInt = min | 0;
-  const sec = 60 * (min - minInt);
-
-  return [degInt, minInt, sec];
-}
-**/
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,74 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const CssWorkletPlugin = require('css-worklet-plugin');
+const WorkerPlugin = require('worker-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { resolve } = require('path');
 
-module.exports = {
-  mode: 'development',
-  target: 'web',
-  entry: {
-    main: './src/index.js'
-  },
-  output: {
-    filename: 'dial.js',
-    path: resolve(__dirname, 'lib'),
-    publicPath: 'lib/'
-  },
-  stats: 'verbose',
-  performance: {
-    hints: 'warning'
-  },
-  resolve: {
-    alias: {
-      'react-dom$': 'react-dom/profiling',
-      'scheduler/tracing': 'scheduler/tracing-profiling',
+module.exports = (env = {})=> {
+  const isDev = env.development;
+  
+  return {
+    mode: isDev ? 'development' : 'production',
+    target: 'web',
+    entry: {
+      main: './src/index.js'
+    },
+    output: {
+      filename: isDev ? '[name].bundle.js' : '[name].[hash].js',
+      path: resolve(__dirname, 'dist'),
+      globalObject: 'this'
+    },
+    devtool: isDev ? 'inline-source-map' : false,
+    devServer: {
+      contentBase: './dist'
+    },
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: isDev ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
+      }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Development',
+        template: 'src/index.html',
+      }),
+      new CssWorkletPlugin(),
+      new WorkerPlugin()
+    ],
+    stats: {
+      all: false,
+      assets: true,
+      builtAt: true,
+      errorDetails: true,
+      errors: true,
+      performance: true,
+    },
+    resolve: {
+      alias: {
+        'react-dom$': 'react-dom/profiling',
+        'scheduler/tracing': 'scheduler/tracing-profiling',
+      }
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        use: [ 'babel-loader' ],
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/i,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            hmr: process.env.NODE_ENV === 'development',
+          }
+        }, 'css-loader'],
+      }]
     }
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      use: [ 'babel-loader' ],
-      exclude: /node_modules/
-    },{
-      test: /\.worker\.js$/,
-      use: [ 'worker-loader' ],
-      exclude: /node_modules/
-    }]
-  }
+  };
 };
